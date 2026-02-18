87fa2385f53243956f82f461148608849ad997ea
Gabriel Birman <estebanbirman2002@gmail.com>
2026-02-18 15:02:44 -0300

    Fix CartProvider wrapper

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,14 @@
-import { createRoot } from "react-dom/client";
+import React from "react";
+import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 
-createRoot(document.getElementById("root")!).render(<App />);
+import { CartProvider } from "./contexts/CartContext";
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <CartProvider>
+      <App />
+    </CartProvider>
+  </React.StrictMode>
+);
